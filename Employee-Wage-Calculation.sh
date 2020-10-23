83bd0d5b87b5c1257f87ca7443c8c11b3813d1e1
Vabs2 <vaibhavkadbhane2@gmail.com>
2020-10-23 10:43:32 +0530

    Checking Daily Wage of the Employee

--- Employee-Wage-Calculation.sh
+++ Employee-Wage-Calculation.sh
@@ -1,6 +1,16 @@
 #! /bin/bash
 
+wagePerHr=20
 isPresent=1
+hrsPerDay=8
+isFullTime=2
+isPartTime=1
+partTimeHrs=4
+fullTimeHrs=8
+noOfWorkingDays=20
+totalWorkHrs=0
+noOfDays=0
+empHrs=0
 
 function attendance()
 {
@@ -11,4 +21,16 @@ function attendance()
                 echo "Employee Is Absent"
         fi
 }
+
+function dailyWage()
+{
+        if [[ $((RANDOM%2)) -eq $isPresent ]]
+        then
+                dailySalary=$(($wagePerHr * $hrsPerDay ))
+                echo "Salary is.."$dailySalary
+        else
+                echo "Salary = 0"
+        fi
+}
 attendance
+dailyWage
